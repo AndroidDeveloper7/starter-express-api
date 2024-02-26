5a6dc772a5032ef121426651e9574a349318c421
Sandeep <161037314+AndroidDeveloper7@users.noreply.github.com>
2024-02-26 23:54:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get("/users", (req,res) => {
+
+    const number = Number(req.query.number);
+    var result = "";
+
+
+    if (number > max) {
+        result = "Pass"
+    } else if (number < min) {
+        result = "Fail"
+    } else {
+        result = "Average"
+    }
+
+
+    return res.json({
+        status : 1,
+        data : "token",
+        result : result
+    })
+});
+
 app.listen(process.env.PORT || 3000)
